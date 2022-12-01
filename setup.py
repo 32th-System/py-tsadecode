e268ab89b307545f9c6f4bcd8d7428fb474d6ac5
32th-System <intensonat@gmail.com>
2022-12-01 22:04:03 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 setup(name="tsadecode",
-      version="0.3",
+      version="0.4",
       description="Decoding utilities for ZUN data",
       author="32th-System",
       author_email="intensonat@gmail.com",
